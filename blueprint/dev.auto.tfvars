2ac98d765d89bf60a305e6d3d5d1c53d7e30426e
Mathew Danish <mathew.danish@genesys.com>
2023-04-13 14:00:18 -0400

    third commitqwe
    Signed-off-by: Mathew Danish <mathew.danish@genesys.com>

--- blueprint/dev.auto.tfvars
+++
@@ -1,4 +0,0 @@
-variable "my_ivr_did_number"{
-  type = string
-  description = "heres a description"
-}
--- blueprint/variable.tf
+++ blueprint/variable.tf
@@ -2,3 +2,5 @@ variable "my_ivr_did_number"{
   type = string
   description = "heres a description"
 }
+
+my_ivr_did_number = "+17204276640"
